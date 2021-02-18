4714df3f0ed384381d474b80474f5578f11e8e35
Nicholas Edwards <nick.edwards@chironix.com>
2021-02-18 16:36:43 +0800

    Switch to fd for finding source directories

--- loc_all.sh
+++ loc_all.sh
@@ -5,11 +5,15 @@ dirs=$(find -mindepth 1 -maxdepth 1 -type d)
 for dir in ${dirs}; do
   echo "--- ${dir} ---" 
 
-  exts=$(find "${dir}" -type f -name "*.*" | sed -e 's/.*\.//' | sort -u)
+  exts=$(
+    fd --type file --hidden --exclude ".git/" . "${dir}" \
+    | sed -e 's/.*\.//' \
+    | sort -u
+  )
 
   for ext in ${exts}; do
     echo -n ".${ext} "
-    find "${dir}" -type f -name "*.${ext}" -print0 \
+    fd --type file --hidden --exclude ".git/" -e ".${ext}" --print0 . "${dir}" \
     | xargs -0 wc -l \
     | tail -1 \
     | sed -E -e 's/^\s*(\S+).*/\1/'
