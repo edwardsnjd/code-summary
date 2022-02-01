a02e193d1d0b05414787315e2724aa1049e5d4a4
Nicholas Edwards <nick.edwards@chironix.com>
2022-02-01 20:55:38 +0800

    chore: Refactor extension whitelist script
    The main change is to extract the whitelist to an array by itself.

--- select_ext.sh
+++ select_ext.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-grep -E "\b(js|jsx|json|ts|tsx|yml|yaml|css|html|py|java|xml)\b|---" -
+# Usage: loc.sh repo1 repo2 | select_ext.sh
+#
+# Limit input to only lines refering to relevant extensions.
+
+# The extensions to keep
+EXT_WHITELIST=(
+ c cpp cs css h html java js json jsx meta prefab
+ py ts tsx xml yaml yml
+)
+
+# Filter stdin to only select lines matching whitelisted extensions
+function SelectWhitelistedExtensions {
+  local extensionsChoice=$( JoinBy "|" "${EXT_WHITELIST[@]}" )
+
+  grep -E "\b(${extensionsChoice})\b"
+}
+
+# Join given positional parameters by the given separator
+function JoinBy {
+  local IFS="$1"
+  shift
+  echo "$*"  # "$*" uses IFS
+}
+
+SelectWhitelistedExtensions "$@"
